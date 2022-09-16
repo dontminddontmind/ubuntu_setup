3c14e54b94e3aa51c73ba6e111df2ef9d6b08e84
dontminddontmind <39674475+dontminddontmind@users.noreply.github.com>
2022-09-16 16:02:17 +0800

    Update 03-setup_zsh.sh

--- 03-setup_zsh.sh
+++ 03-setup_zsh.sh
@@ -18,14 +18,14 @@ sudo chsh -s /bin/zsh root
 
 
 #安装powerlevel10k主题
-git clone https://hub.fastgit.org/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-#git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+#git clone https://hub.fastgit.org/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 #安装插件
 #根据 shell 中输入命令的历史记录，为当前输入的命令提供自动补全提示
-git clone https://hub.fastgit.org/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-#git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+#git clone https://hub.fastgit.org/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 #为 shell 命令进行语法高亮显示。
-git clone https://hub.fastgit.org/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+#git clone https://hub.fastgit.org/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
