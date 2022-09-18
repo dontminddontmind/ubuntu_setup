239084d9a0980c5e9dcd7de5ee3eb867d323dae9
dontminddontmind <39674475+dontminddontmind@users.noreply.github.com>
2022-09-18 16:49:19 +0800

    Update 00-dot_write.sh

--- 00-dot_write.sh
+++ 00-dot_write.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#复制配置文件
-#怎么改username呀！
+#怎么更好的改username呀！
 sed -i '/ctf-tools/d'   .bashrc
 echo "# ctf-tools: PATH setup" >>.bashrc
 echo "export PATH=/home/${USER}/tools/ctf-tools/bin:\$PATH" >>.bashrc
@@ -11,6 +10,5 @@ echo "export PATH=/home/${USER}/tools/ctf-tools/bin:\$PATH" >>.zshrc
 # sed -i '/virtualenvwrapper.sh/d'   ./etc/profile.d/virtualenvwrapper.sh
 # echo "source /home/${USER}/.local/bin/virtualenvwrapper.sh" >> ./etc/profile.d/virtualenvwrapper.sh
 
-cp ./.bashrc ./.zshrc ./.p10k.zsh ~/
 
 		
