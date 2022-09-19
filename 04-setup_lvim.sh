281bfa64ac9d65eab9001c0cc893e49818e1fb98
dontminddontmind <39674475+dontminddontmind@users.noreply.github.com>
2022-09-19 21:09:17 +0800

    Update 04-setup_lvim.sh

--- 04-setup_lvim.sh
+++ 04-setup_lvim.sh
@@ -17,30 +17,30 @@ chmod 777 ./install.sh
 rm ./install.sh
 
 #wsl https://www.lunarvim.org/01-installing.html#tips-for-wsl-2-users
-if test  ! -z "$(uname -a|grep WSL)" ;then
-        #sudo echo权限不够
-        #https://www.shellhacks.com/sudo-echo-to-file-permission-denied/
-        if sudo [ ! -f /etc/wsl.conf ]
-        then
-                sudo touch /etc/wsl.conf
-        fi
-        if sudo test -z "$(cat /etc/wsl.conf|grep '# lunarvim')" ;then
-                echo -e "\n\
-# lunarvim\n\
-[automount]\n\
-# Set to true will automount fixed drives (C:/ or D:/) with DrvFs under the root directory set above. Set to false means drives won't be mounted automatically, but need to be mounted manually or with fstab.\n\
-enabled = false\n\
-\n\
-# Sets the \`/etc/fstab\` file to be processed when a WSL distribution is launched.\n\
-mountFsTab = false\n\
-\n\
-# Set whether WSL supports interop process like launching Windows apps and adding path variables. Setting these to false will block the launch of Windows processes and block adding \$PATH environment variables.\n\
-[interop]\n\
-enabled = false\n\
-appendWindowsPath = false\n\
-    " | sudo tee --append /etc/wsl.conf
-        fi
+# if test  ! -z "$(uname -a|grep WSL)" ;then
+#         #sudo echo权限不够
+#         #https://www.shellhacks.com/sudo-echo-to-file-permission-denied/
+#         if sudo [ ! -f /etc/wsl.conf ]
+#         then
+#                 sudo touch /etc/wsl.conf
+#         fi
+#         if sudo test -z "$(cat /etc/wsl.conf|grep '# lunarvim')" ;then
+#                 echo -e "\n\
+# # lunarvim\n\
+# [automount]\n\
+# # Set to true will automount fixed drives (C:/ or D:/) with DrvFs under the root directory set above. Set to false means drives won't be mounted automatically, but need to be mounted manually or with fstab.\n\
+# enabled = false\n\
+# \n\
+# # Sets the \`/etc/fstab\` file to be processed when a WSL distribution is launched.\n\
+# mountFsTab = false\n\
+# \n\
+# # Set whether WSL supports interop process like launching Windows apps and adding path variables. Setting these to false will block the launch of Windows processes and block adding \$PATH environment variables.\n\
+# [interop]\n\
+# enabled = false\n\
+# appendWindowsPath = false\n\
+#     " | sudo tee --append /etc/wsl.conf
+#         fi
 
-fi
+# fi
 
 #TODO update
