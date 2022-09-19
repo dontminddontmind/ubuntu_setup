d995f2a62e084645a627b09906fea16a6287a624
dontminddontmind <39674475+dontminddontmind@users.noreply.github.com>
2022-09-19 19:40:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 #按照顺序依次执行setup文件
 #复制我的配置
 cp ./.bashrc ./.zshrc ./.p10k.zsh ~/ 
+mkdir ~/.config
+mkdir ~/.config/lvim
 cp ./.config/lvim/* ~/.config/lvim/  
 # 编辑etc/profile.d/proxy.sh文件，把代理地址、协议和端口改成你的
 #配置网络，apt镜像源，代理
